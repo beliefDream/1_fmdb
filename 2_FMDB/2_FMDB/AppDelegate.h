052d96c63273cdd1c525a0006ea1b04c44dcc39e
dream <781178325@qq.com>
2015-10-30 09:54:04 +0800

    初次提交工程

---
+++ 2_FMDB/2_FMDB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  2_FMDB
+//
+//  Created by 黄纪超 on 15/10/30.
+//  Copyright (c) 2015年 黄纪超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
