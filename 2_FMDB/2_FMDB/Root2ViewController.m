143a0f75c47a6d15e9ab06c1ceddddd03503dfd9
dream <781178325@qq.com>
2015-10-30 23:15:23 +0800

    输入文本框没响应

--- 2_FMDB/2_FMDB/Root2ViewController.m
+++ 2_FMDB/2_FMDB/Root2ViewController.m
@@ -7,8 +7,17 @@
 //
 
 #import "Root2ViewController.h"
+#import <FMDB.h>
 
 @interface Root2ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *nameTF;
+@property (weak, nonatomic) IBOutlet UITextField *ageTF;
+@property (weak, nonatomic) IBOutlet UITextField *numberTF;
+- (IBAction)insertData:(id)sender;
+- (IBAction)deleteData:(id)sender;
+- (IBAction)modifyData:(id)sender;
+- (IBAction)queryData:(id)sender;
+@property (nonatomic, strong)FMDatabase * db;
 
 @end
 
@@ -17,7 +26,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+ 
+    NSString * filePath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    NSString * dbPath = [filePath stringByAppendingPathComponent:@"db.sqlite"];
+    NSLog(@"%@", dbPath);
+    self.db = [FMDatabase databaseWithPath:dbPath];
+    
+    [self createTable];
 }
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -34,4 +51,69 @@
 }
 */
 
+- (FMDatabase *)db {
+    if (_db == nil) {
+        NSString * filePath = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+        NSString * dbPath = [filePath stringByAppendingPathComponent:@"db.sqlite"];
+        NSLog(@"%@", dbPath);
+        self.db = [FMDatabase databaseWithPath:dbPath];
+        
+    }
+    return _db;
+}
+
+- (void)createTable {
+    if ([self.db open]) {
+        NSString * createSql = @"CREATE TABLE  IF NOT EXISTS PersonList (Name text, Age integer,  Sex integer, Phone text, Address text, Photo blob)";
+        BOOL isExecute = [_db executeUpdate:createSql];
+        if (!isExecute) {
+            NSLog(@" ... error");
+        }
+        [_db close];
+    }
+}
+
+- (IBAction)insertData:(id)sender {
+    if ([self.db open]) {
+//        NSString * insertSql = @"INSERT INTO  PersonList (Name, Age, Sex) VALUES (?, ?, ?)";
+//
+//        [_db executeUpdate:@"INSERT INTO  PersonList (Name) VALUES (?, ?, ?)",
+//        /* _nameTF.text, _ageTF.text, _numberTF.text*/ @"11", @11, @11];
+//        [_db executeUpdate:@"INSERT INTO  PersonList (Name) VALUES (? )", @"aa"];
+        NSString *sql = @"INSERT INTO PersonList (Name, Age) VALUES (?, ?)";
+             [_db executeUpdate:sql, @"sdsd", _ageTF.text];
+   
+        [_db close];
+    }
+}
+
+- (IBAction)deleteData:(id)sender {
+    if ([self.db open]) {
+        NSString * deleteSql = @"delete from PersonList";
+        [_db executeUpdate:deleteSql];
+        [_db close];
+    }
+}
+
+- (IBAction)modifyData:(id)sender {
+    if ([self.db open]) {
+        [_db executeUpdate:@"UPDATE PersonList SET Age = ? WHERE Name = ?",
+        [NSNumber numberWithInt:30],@"sdsd"];
+        [_db close];
+    }
+}
+
+- (IBAction)queryData:(id)sender {
+    if ([self.db open]) {
+        NSString * queryStr = @"SELECT Name, Age FROM PersonList";
+        FMResultSet * rs = [_db executeQuery:queryStr];
+        if ([rs next]) {
+           NSString * name = [rs stringForColumn:@"Name"];
+           int age = [rs intForColumn:@"Age"];
+            int kk = 0;
+            NSLog(@"name = %@, age = %d, number =  %d\n\n", name, age, kk);
+        }
+        [_db close];
+    }
+}
 @end
